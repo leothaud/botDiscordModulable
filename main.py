5ddf2ddb06ba5dc3ecb8fe76b62d35139e60f7ec
leothaud <72204953+leothaud@users.noreply.github.com>
2021-02-06 16:20:30 +0100

    ajout, listage et récupération d'infos possible pour les commandes

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 import discord
 import asyncio
 import sqlite3
+import ast
+
 
 token = None
+idGestion = None
 startChar = '\\'
 dbName = "example.db"
+currentMessage = None
 
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 
+def deGestionnaire(msg):
+    global idGestion
+    roles = msg.author.roles
+    res = False
+    for role in roles:
+        if role.id == idGestion:
+            res = True
+    return res
+    
 
+async def async_exec(stmts, env=None):
+    parsed_stmts = ast.parse(stmts)
+
+    fn_name = "_async_exec_f"
+
+    fn = f"async def {fn_name}(): pass"
+    parsed_fn = ast.parse(fn)
+
+    for node in parsed_stmts.body:
+        ast.increment_lineno(node)
+
+    parsed_fn.body[0].body = parsed_stmts.body
+    exec(compile(parsed_fn, filename="<ast>", mode="exec"), env)
+
+    return await eval(f"{fn_name}()", env)
+
+async def send(text):
+    global currentMessage
+    n = len(text)//2000
+    for i in range(n+1):
+        tmp = text[2000*i:2000*(i+1)]
+        await currentMessage.channel.send(tmp)
 
 def nameCommand(msg):
     """
@@ -54,7 +89,8 @@ def nameArgs(argsString):
         k = i
     return names
         
-    
+
+# \command (n0)nom (n1)[fkgfkdpgf](n2)   
 
 async def treat(msg):
     """
@@ -67,10 +103,78 @@ async def treat(msg):
     elif msg.content[1:] == "count":
         nbr = howMuch(msg.author.id)
         await msg.channel.send("Tu as envoyé "+str(nbr[0])+" messages.")
+    elif msg.content[1:] == "listCommand":
+        conn = sqlite3.connect(dbName)
+        c = conn.cursor()
+        c.execute("SELECT nom FROM functions;")
+        liste = c.fetchall()
+        text = ""
+        for elt in liste:
+            text += elt[0]+"\n"
+        await send(text)
+        conn.close()
     else:
         commandName = nameCommand(msg.content)
-        if commandName == "addCommand":
-            pass
+        if commandName == "infoCommand":
+            i = 0
+            while (i < len(msg.content) and msg.content[i] != " "):
+                i += 1
+            if i != len(msg.content):
+                i += 1
+                n0 = i
+                while (i < len(msg.content) and msg.content[i] != " "):
+                    i += 1
+                functionName = msg.content[n0:i]
+                conn = sqlite3.connect(dbName)
+                c = conn.cursor()
+                c.execute("SELECT * FROM functions WHERE nom='"+functionName+"';")
+                res = c.fetchone()
+                if res == None:
+                    await send("Commande "+functionName+" introuvable.")
+                else:
+                    await send(res[1]+" "+res[2]+" : ```"+res[3]+"```")
+            else:
+                await send("Récupération informations impossibles")
+        elif commandName == "addCommand":
+            if deGestionnaire(msg):
+                i = 0
+                while (i < len(msg.content) and msg.content[i] != " "):
+                    i += 1
+                if i != len(msg.content):
+                    i += 1
+                    n0 = i
+                    while (i < len(msg.content) and msg.content[i] != " "):
+                        i += 1
+                    if i != len(msg.content):
+                        i += 1
+                        n1 = i
+                        while (i < len(msg.content) and msg.content[i] != " "):
+                            i += 1
+                        if i != len(msg.content):
+                            i += 1
+                            n2 = i
+                            functionName = msg.content[n0:n1-1]
+                            functionArgs = msg.content[n1:n2-1]
+                            function = msg.content[n2+3:-3]
+                            conn = sqlite3.connect(dbName)
+                            c = conn.cursor()
+                            c.execute("SELECT COUNT(*) FROM functions WHERE nom=\'"+functionName+"\';")
+                            res = c.fetchone()
+                            if res[0] == 0:
+                                c.execute("INSERT INTO functions (nom, args, command) VALUES (\'"+functionName+"\',\'"+functionArgs+"\',\'"+function+"\');")
+                                conn.commit()
+                                await send("Fonction "+functionName+" ajoutée avec succès.")
+                            else:
+                                await send("Fonction "+functionName+" déjà existante.")
+                            conn.close()
+                        else:
+                            await msg.channel.send("Cette commande prend comme arguments le nom de la fonction, la liste des arguments puis la fonction entre `.")
+                    else:
+                        await msg.channel.send("Cette commande prend comme arguments le nom de la fonction, la liste des arguments puis la fonction entre `.")
+                else:
+                    await msg.channel.send("Cette commande prend comme arguments le nom de la fonction, la liste des arguments puis la fonction entre `.")
+            else:
+                await msg.channel.send("Tu n'as pas le droit.")
         else:
             args = argsCommand(msg.content)
             conn = sqlite3.connect(dbName)
@@ -86,10 +190,10 @@ async def treat(msg):
                 if len(argsName) != len(args):
                     await msg.channel.send("Cette commande prend exactement " + str(len(argsName)) + " argument" + ("" if len(argsName)<2 else "s") + ".")
                 else:
-                    Parameter = {}
+                    Parameter = dict(globals())
                     for i in range(len(argsName)):
                         Parameter[argsName[i]] = eval(args[i])
-                    exec(command[2],Parameter)
+                    await async_exec(command[2],Parameter)
             else:
                 await msg.channel.send("Commande inconnue.")
             conn.close()
@@ -129,11 +233,13 @@ def tokChan():
         Cette fonction récupère le token du bot et l'id du channel dans un fichier séparé.
     """
     global token
+    global idGestion
     doc = open('token', 'r')
     lines = []
     for line in doc:
         lines.append(line)
     token = lines[0]
+    idGestion = int(lines[1])
 
 async def main():
     """
@@ -147,8 +253,10 @@ async def on_message(msg):
     """
         Le code ici s'exécute quand le bot voit un nouveau message.
     """
+    global currentMessage
     countMessage(msg)
-    if msg.content[0] == startChar:
+    if len(msg.content)>0 and msg.content[0] == startChar:
+        currentMessage = msg
         await treat(msg)
         await msg.add_reaction("\N{lollipop}")
         
